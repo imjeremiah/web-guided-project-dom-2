4aa31f05a728b939d106ae798ceb31c6fff8029b
Casey Harding <casey.r.harding@gmail.com>
2021-11-30 13:05:56 -0500

    Through Task 5.

--- src/index.js
+++ src/index.js
@@ -31,16 +31,37 @@ launchButton.addEventListener("click", function(evt) {
 // 👉 TASK 3- Create a function that launches!
 // It should open the confirmation modal.
 // Add it as an event listener for click events on the launch button.
-
+function launch() {
+  modal.classList.remove("off");
+  successMessage.classList.add("off");
+  failureMessage.classList.add("off");
+}
+launchButton.addEventListener("click", launch)
 
 // 👉 TASK 4- Create a function to confirm the launch.
 // It should close the modal and display a success report.
 // Add it as a listener for clicks on the confirmation button.
-
+function confirm() {
+  modal.classList.add("off");
+  successMessage.classList.remove("off");
+}
+confirmButton.addEventListener("click", confirm);
 
 // 👉 TASK 5- Create a function to cancel the launch.
 // It should close the modal and display a failure report.
 // Add it as a listener for clicks on the cancellation button.
+function cancel() {
+  modal.classList.add("off");
+  failureMessage.classList.remove("off");
+}
+cancelButton.addEventListener("click", cancel);
+
+/**
+ * function declaration => function foo() {} => Hoisted!
+ * function expression =>  const foo = () => {} => Static!
+ */
+
+
 
 
 // 👉 TASK 6- Create a function that closes the modal if
